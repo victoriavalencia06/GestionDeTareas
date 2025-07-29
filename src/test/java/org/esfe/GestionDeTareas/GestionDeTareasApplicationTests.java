c41d2c6576fe26718428590c4515377f10959395
Victoria Reyes <victoriavalencia2006@outlook.com>
2025-07-29 15:39:53 -0600

    Estructra del proyecto y configuracion del layout

---
+++ src/test/java/org/esfe/GestionDeTareas/GestionDeTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esfe.GestionDeTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
