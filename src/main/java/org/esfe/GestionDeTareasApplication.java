50926591222bcb605299e5f0b863e166650390c5
Victoria Reyes <victoriavalencia2006@outlook.com>
2025-07-30 17:39:04 -0600

    Funcionalidad completa para el modelo Grupo

--- src/main/java/org/esfe/GestionDeTareasApplication.java
+++ src/main/java/org/esfe/GestionDeTareasApplication.java
@@ -1,4 +1,4 @@
-package org.esfe.GestionDeTareas;
+package org.esfe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/org/esfe/modelos/Grupo.java
@@ -0,0 +1,44 @@
+package org.esfe.modelos;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "Grupos")
+public class Grupo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre es requerido")
+    private String nombre;
+
+    @NotBlank(message = "La descripción es requerida")
+    @Size(max = 50, message = "Máximo 50 caracteres")
+    private String descripcion;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
