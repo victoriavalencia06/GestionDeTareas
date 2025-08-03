56097675b866a658940cbad268b66421aadb1d2e
Victoria Reyes <victoriavalencia2006@outlook.com>
2025-08-03 12:51:16 -0600

    Implementación de Spring Security

---
+++ src/main/java/org/esfe/security/DatabaseWebSecurity.java
@@ -0,0 +1,29 @@
+package org.esfe.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class DatabaseWebSecurity {
+    @Bean
+    public UserDetailsManager customUsers(DataSource dataSource){
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.setUsersByUsernameQuery("select login, clave, status from usuarios where login = ?");
+        users.setAuthoritiesByUsernameQuery("select u.login, r.nombre from usuario_rol ur " +
+                "inner join usuarios u on u.id = ur.usuario_id " +
+                "inner join roles r on r.id = ur.rol_id " +
+                "where u.login = ?");
+
+        return users;
+    }
+}
